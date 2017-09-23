035a42bd4e73c64dbf964e6fad47e74f3f805a14
aneeshverenkar <aneesh.verenkar@gmail.com>
2017-09-23 17:01:38 -0500

    Initial Project Setup

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('/public'));
+
+
+app.listen(3000);
+app.engine('html', require('ejs').renderFile);
+
+app.set('views', './public' + '/views');
+
+app.get('/', function(req, res) {
+    res.render('home.html');
+    });
+
