9bf0805fc291ac6abb20af3a7edd1c3aa2787278
aneeshverenkar <aneesh.verenkar@gmail.com>
2017-09-23 20:33:59 -0500

    change

--- endpoints.js
+++ endpoints.js
@@ -3,7 +3,7 @@ module.exports = Endpoints;
 function Endpoints(app){
 
     app.post('/foodRequest', function(req, res) {
-        console.log(request.body.request);
+        //console.log(request.body.request);
         
              res.send("OK!");
         });
