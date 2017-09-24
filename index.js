b61ff039478c1780cb25be27737e06ee0eafe94b
aneeshverenkar <aneesh.verenkar@gmail.com>
2017-09-23 23:11:41 -0500

    fix

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 app.use(express.static('/public'));
 
@@ -7,6 +8,9 @@ var port = process.env.port || 3000
 
 app.listen(port);
 app.engine('html', require('ejs').renderFile);
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.set('views', './public' + '/views');
 
