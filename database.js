c26624369b4cda5a231c4499bc8a54ba0ff8cf2c
Alex Richardson <alexrichardsonschool@gmail.com>
2017-09-24 04:34:21 -0400

    Added basic database code.

---
+++ database.js
@@ -0,0 +1,54 @@
+const goose = require('mongoose');
+goose.connect('mongodb://localhost/USER_DATA', {useMongoClient: true});
+
+var Schema = goose.Schema;
+var ObjectId = Schema.ObjectId;
+
+var Auth = new Schema ({
+	email: String
+}, {collection: 'Auth'});
+
+var Locations = new Schema ({ 
+	email: String,
+	location: String
+}, {collection: 'Locations'});
+
+var KeyWords = new Schema ({
+	email: String
+}, {collection: 'KeyWords'} );
+
+goose.model('Auth', Auth);
+goose.model('Locations', Locations);
+goose.model('KeyWords', KeyWords);
+	
+	
+	
+	
+exports.CheckEmail = function (email, callback) { 
+	goose.model('Auth').findOne({'email' : email}, function (err, doc) {
+		if (err) { 
+			callback(false);
+		}
+		else { 
+			callback(true);
+		};
+	});
+};
+
+
+exports.GetLocation = function (email, callback) { 
+	goose.model('Locations').findOne({'email' : email}, function (err, doc) { 
+		if (err) { 
+			callback();
+		}
+		else { 
+			callback(doc);
+		};
+	});
+};
+
+exports.GetLocation("testingthis@mail.net", function (doc) { 
+	if (doc) { 
+		console.log(doc.location);
+	};
+});
