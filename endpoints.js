9ea42f0b2848dddfd280792a3f9db1722752a54c
Alex Richardson <alexrichardsonschool@gmail.com>
2017-09-24 06:19:49 -0400

    Possible improvement?

--- endpoints.js
+++ endpoints.js
@@ -40,7 +40,7 @@ function Endpoints(app){
 		     
 		  //var location = database.GetLocation("testingthis@mail.net");   
 		  var location = "16551 Dora Hamann Pkwy, Omaha NE 68116";
-          var restaurants = yelpAPI.SearchForBusinesses(location, price, intents.entities[0].entity, intents.entities[1].entity);
+          var restaurants = yelpAPI.SearchForBusinesses(location, price, intents.entities[0].entity, intents.entities[2].entity);
           // console.log(restaurants.businesses[0].name);
 		  
            var reply = 'There\'s ' +restaurants.businesses[0].name +' at '+restaurants.businesses[0].location.address1+'. It has a '+restaurants.businesses[0].rating+' star rating.';
