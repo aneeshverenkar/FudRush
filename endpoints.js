9c7c370b2f2e70c28d11bdf254dab5d176f3d08d
Alex Richardson <alexrichardsonschool@gmail.com>
2017-09-24 04:45:00 -0400

    Account is not as hardcoded.

--- endpoints.js
+++ endpoints.js
@@ -1,6 +1,7 @@
 module.exports = Endpoints;
 var rp = require('request-promise');
 var yelpAPI = require('./yelp.js');
+var database = require('./database.js');
 
 function Endpoints(app){
 
@@ -36,7 +37,9 @@ function Endpoints(app){
               }
 
                //Yelp it!
-          var restaurants = yelpAPI.SearchForBusinesses('500 S State St, Ann Arbor, MI 48109', price, intents.entities[0].entity);
+		     
+		  var location = database.GetLocation("testingthis@mail.net");   
+          var restaurants = yelpAPI.SearchForBusinesses(location, price, intents.entities[0].entity);
           // console.log(restaurants.businesses[0].name);
 
            var reply = 'There\'s ' +restaurants.businesses[0].name +' at '+restaurants.businesses[0].location.address1+'. It has a '+restaurants.businesses[0].rating+' star rating.';
